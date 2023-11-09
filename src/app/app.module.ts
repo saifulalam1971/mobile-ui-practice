63b5794595c2981c04a1a4381553f53107c256c1
saifulalam1971 <saifulalam85902@gmail.com>
2023-11-10 02:04:57 +0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
+import { NativeScriptCommonModule, NativeScriptFormsModule, NativeScriptModule } from '@nativescript/angular'
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { ItemsComponent } from './item/items.component'
+import { ItemDetailComponent } from './item/item-detail.component'
+import { NativeScriptMaterialButtonModule } from "@nativescript-community/ui-material-button/angular";
+import { ReactiveFormsModule } from '@angular/forms'
+
+@NgModule({
+  bootstrap: [AppComponent],
+  imports: [
+    NativeScriptModule, 
+    NativeScriptCommonModule,
+    NativeScriptModule,
+    ReactiveFormsModule,
+    NativeScriptFormsModule,
+    AppRoutingModule,
+    NativeScriptMaterialButtonModule
+  ],
+  declarations: [AppComponent, ItemsComponent, ItemDetailComponent],
+  providers: [],
+  schemas: [NO_ERRORS_SCHEMA],
+})
+export class AppModule {}
