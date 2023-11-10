1e53d099e3cf83e6a6a7d64127cb5ca4c0cfbf04
saifulalam1971 <saifulalam85902@gmail.com>
2023-11-10 11:18:19 +0600

    image upload

--- src/app/home/details/details.component.ts
+++ src/app/home/details/details.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { Item } from './../../item/item'
 import { ItemService } from './../../item/item.service'
-import * as camera from "nativescript-camera";
+import * as camera from "@nativescript/camera";
+import { ImageAsset } from '@nativescript/core';
 @Component({
     selector: 'ns-details',
     templateUrl: './details.component.html',
     styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-  item: Item
+  item: Item;
+  img:any;
 
   constructor(private itemService: ItemService, private route: ActivatedRoute) {}
 
@@ -18,25 +20,27 @@ export class DetailsComponent implements OnInit {
     this.item = this.itemService.getItem(id)
   }
   
-  openCamera() {
-  }
-//     let payload = {};
-//     const options = {
-//       width: 300,
-//       height: 300,
-//       keepAspectRatio: true,
-//       saveToGallery: false
-//     };
 
-//     camera.requestPermissions().then(()=>{  
-//       camera.takePicture(options)
-//       .then((imageAsset: any) => {
-//         console.log("got file from camera", imageAsset);
-//       }).catch(err => {
-//         console.log(err.message);
-//       });
-//      }).catch(err=> {
-//        console.log(err.message);
-//      })
-//   }
+  openCamera() {
+    let payload = {};
+    const options = {
+      width: 300,
+      height: 300,
+      keepAspectRatio: true,
+      saveToGallery: false
+    };
+
+    camera.requestPermissions().then(()=>{  
+        camera.takePicture(options)
+        .then((imageAsset: ImageAsset) => {
+          console.log("got file from camera", imageAsset);
+        //   this.img=imageAsset;
+
+        }).catch(err => {
+          console.log(err.message);
+        });
+       }).catch(err=> {
+         console.log(err.message);
+       })
+  }
 }
